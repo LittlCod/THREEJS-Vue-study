c7bcc0cb81151cb1dedf6e2b5412ba26afc8aa6e
苏昊远 <1109069349@qq.com>
2023-11-24 10:18:57 +0800

    增加菜单

--- src/router/asyncRoutes.js
+++ src/router/asyncRoutes.js
@@ -1,50 +1,50 @@
 const asyncRoutes = [
-    // {
-    //     path: '/home',
-    //     name: 'home',
-    //     meta: { title: '首页', icon: 'House' },
-    //     component: () => import("@/views/home/IndexView.vue")
-    // },
+    {
+        path: '/home',
+        name: 'home',
+        meta: { title: '首页', icon: 'House' },
+        component: () => import("@/views/0-menu.vue"),
+    },
     {
         path: '/1',
         name: '1',
-        meta: { title: '创建第一个three.js页面'},
+        meta: { title: '1.创建第一个three.js页面'},
         component: () => import("@/views/1-first.vue"),
     },
     {
         path: '/2',
         name: '2',
-        meta: { title: '物体移动和旋转等'},
+        meta: { title: '2.物体移动和旋转等'},
         component: () => import("@/views/2-move.vue"),
     },
     {
         path: '/3',
         name: '3',
-        meta: { title: '自适应窗口大小'},
+        meta: { title: '3.自适应窗口大小'},
         component: () => import("@/views/3-adaptive.vue"),
     },
     {
         path: '/4',
         name: '4',
-        meta: { title: '使用GUI控制物体'},
+        meta: { title: '4.使用GUI控制物体'},
         component: () => import("@/views/4-gui.vue"),
     },
     {
         path: '/5',
         name: '5',
-        meta: { title: '创建几何体'},
+        meta: { title: '5.创建几何体'},
         component: () => import("@/views/5-几何体.vue"),
     },
     {
         path: '/6',
         name: '6',
-        meta: { title: '材质分组'},
+        meta: { title: '6.材质分组'},
         component: () => import("@/views/6-材质分组.vue"),
     },
     {
         path: '/7',
         name: '7',
-        meta: { title: '材质贴图'},
+        meta: { title: '7.材质贴图'},
         component: () => import("@/views/7-材质贴图.vue"),
     },
 ];
---
+++ src/views/0-menu.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="menu">
+        <div class="item" v-for="item in asyncRoutes.slice(1)" @click="go(item)">
+            {{ item.meta.title }}
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted, computed, watch } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import asyncRoutes from "@/router/asyncRoutes";
+
+const $router = useRouter();
+const go = item => {
+    $router.push(item.path);
+};
+</script>
+
+<style lang="scss" scoped>
+.menu{
+    margin: 50px 150px;
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    gap: 10px;
+    .item{
+        padding: 20px;
+        background-color: brown;
+        text-align: center;
+        cursor: pointer;
+        color: aliceblue;
+    }
+}
+</style>
